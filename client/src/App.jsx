c00f428f999ba784e4fbc6251480b24ba26885db
Tamirat <amenguda@gmail.com>
2024-03-11 21:21:44 +0300

    create client vite react app and inatall all library

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+      <div className=' bg-black'>
+       
+      </div>
+      
+    
+  )
+}
+
+export default App
---
+++ client/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      gridTemplateRows: {
+        '[auto,auto,1fr]': 'auto auto 1fr',
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/aspect-ratio'),require('@tailwindcss/forms')],
+};
+
