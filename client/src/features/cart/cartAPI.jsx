f5c082dceb5cc3ea01705f09216b79c312795b6d
Tamirat <amenguda@gmail.com>
2024-03-13 08:49:23 +0300

    create Cart and cratAPI

---
+++ client/src/features/cart/cartAPI.jsx
@@ -0,0 +1,54 @@
+export function addToCart(item) {
+  return new Promise(async (resolve) => {
+    const response = await fetch('/backend/cart', {
+      method: 'POST',
+      body: JSON.stringify(item),
+      headers: { 'content-type': 'application/json' },
+    });
+    const data = await response.json();
+    resolve({ data });
+  });
+}
+
+export function fetchItemsByUserId() {
+  return new Promise(async (resolve) => {
+    const response = await fetch('/backend/cart');
+    const data = await response.json();
+    resolve({ data });
+  });
+}
+
+export function updateCart(update) {
+  return new Promise(async (resolve) => {
+    const response = await fetch('/backend/cart/' + update.id, {
+      method: 'PATCH',
+      body: JSON.stringify(update),
+      headers: { 'content-type': 'application/json' },
+    });
+    const data = await response.json();
+    resolve({ data });
+  });
+}
+
+export function deleteItemFromCart(itemId) {
+  return new Promise(async (resolve) => {
+    const response = await fetch('/backend/cart/' + itemId, {
+      method: 'DELETE',
+      headers: { 'content-type': 'application/json' },
+    });
+    const data = await response.json();
+    resolve({ data: { id: itemId } });
+  });
+}
+
+export function resetCart() {
+  // get all items of user's cart - and then delete each
+  return new Promise(async (resolve) => {
+    const response = await fetchItemsByUserId();
+    const items = response.data;
+    for (let item of items) {
+      await deleteItemFromCart(item.id);
+    }
+    resolve({ status: 'success' });
+  });
+}
