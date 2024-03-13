ed920ae8875195b6a6fda4805122e39c79ecbbd6
Tamirat <amenguda@gmail.com>
2024-03-13 08:48:43 +0300

    created order page order API and orderSlice

---
+++ client/src/features/order/orderAPI.jsx
@@ -0,0 +1,43 @@
+export function createOrder(order) {
+  return new Promise(async (resolve) => {
+    const response = await fetch('/backend/orders', {
+      method: 'POST',
+      body: JSON.stringify(order),
+      headers: { 'content-type': 'application/json' },
+    });
+    const data = await response.json();
+    resolve({ data });
+  });
+}
+
+export function updateOrder(order) {
+  return new Promise(async (resolve) => {
+    const response = await fetch('/backend/orders/'+order.id, {
+      method: 'PATCH',
+      body: JSON.stringify(order),
+      headers: { 'content-type': 'application/json' },
+    });
+    const data = await response.json();
+    resolve({ data });
+  });
+}
+
+export function fetchAllOrders(sort, pagination) {
+ let queryString = '';
+
+ for (let key in sort) {
+  queryString += `${key}=${sort[key]}&`;
+}
+  for (let key in pagination) {
+    queryString += `${key}=${pagination[key]}&`;
+  }
+
+  return new Promise(async (resolve) => {
+    const response = await fetch(
+      '/backend/orders?' + queryString
+    );
+    const data = await response.json();
+    const totalOrders = await response.headers.get('X-Total-Count');
+    resolve({ data: { orders: data, totalOrders: +totalOrders } });
+  });
+}
---
+++ client/src/features/order/orderSlice.js
@@ -0,0 +1,82 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { createOrder, fetchAllOrders,updateOrder } from './orderAPI';
+
+const initialState = {
+  orders: [],
+  status: 'idle',
+  currentOrder: null,
+  totalOrders: 0
+};
+//we may need more info of current order
+
+export const createOrderAsync = createAsyncThunk(
+  'backend/order/createOrder',
+  async (order) => {
+    const response = await createOrder(order);
+    // The value we return becomes the `fulfilled` action payload
+    return response.data;
+  }
+);
+export const updateOrderAsync = createAsyncThunk(
+  'backend/order/updateOrder',
+  async (order) => {
+    const response = await updateOrder(order);
+    // The value we return becomes the `fulfilled` action payload
+    return response.data;
+  }
+);
+
+export const fetchAllOrdersAsync = createAsyncThunk(
+  'backend/order/fetchAllOrders',
+  async ({sort, pagination}) => {
+    const response = await fetchAllOrders(sort,pagination);
+    // The value we return becomes the `fulfilled` action payload
+    return response.data;
+  }
+);
+
+export const orderSlice = createSlice({
+  name: 'order',
+  initialState,
+  reducers: {
+    resetOrder: (state) => {
+      state.currentOrder = null;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(createOrderAsync.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(createOrderAsync.fulfilled, (state, action) => {
+        state.status = 'idle';
+        state.orders.push(action.payload);
+        state.currentOrder = action.payload;
+      })
+      .addCase(fetchAllOrdersAsync.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchAllOrdersAsync.fulfilled, (state, action) => {
+        state.status = 'idle';
+        state.orders = action.payload.orders;
+        state.totalOrders = action.payload.totalOrders;
+      })
+      .addCase(updateOrderAsync.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(updateOrderAsync.fulfilled, (state, action) => {
+        state.status = 'idle';
+        const index =  state.orders.findIndex(order=>order.id===action.payload.id)
+        state.orders[index] = action.payload;
+      })
+  },
+});
+
+export const { resetOrder } = orderSlice.actions;
+
+export const selectCurrentOrder = (state) => state.order.currentOrder;
+export const selectOrders = (state) => state.order.orders;
+export const selectTotalOrders = (state) => state.order.totalOrders;
+export const selectStatus = (state) => state.order.status;
+
+export default orderSlice.reducer;
