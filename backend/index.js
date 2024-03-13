5cd284a78d23884bbefc19c635065f858f948721
Tamirat <amenguda@gmail.com>
2024-03-14 01:34:31 +0300

    create backend index file

---
+++ backend/index.js
@@ -0,0 +1,209 @@
+require('dotenv').config();
+const express = require('express');
+const server = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const cookieParser = require('cookie-parser');
+const { createProduct } = require('./controller/Product');
+const productsRouter = require('./routes/Products');
+const categoriesRouter = require('./routes/Categories');
+const brandsRouter = require('./routes/Brands');
+const usersRouter = require('./routes/Users');
+const authRouter = require('./routes/Auth');
+const cartRouter = require('./routes/Cart');
+const ordersRouter = require('./routes/Order');
+const { User } = require('./model/User');
+const { isAuth, sanitizeUser, cookieExtractor } = require('./services/common');
+const path = require('path');
+const { Order } = require('./model/Order');
+
+// // Webhook
+// const endpointSecret = process.env.STRIPE_ENDPOINT_SECRET;
+const endpoint_secret = 'whsec_ea228181b7f44647650502fa8c52bf26ab55a37211a02db3113c1ce1371154b7'
+
+server.post(
+  '/backend/webhook',
+  express.raw({ type: 'application/json' }),
+  async (request, response) => {
+    const sig = request.headers['stripe-signature'];
+
+    let event;
+
+    try {
+      event = stripe.webhooks.constructEvent(request.body, sig, endpoint_secret);
+    } catch (err) {
+      console.error('Webhook Error:', err.message);
+      response.status(400).send(`Webhook Error: ${err.message}`);
+      return;
+    }
+
+    // Handle the event
+    switch (event.type) {
+      case 'payment_intent.succeeded':
+        const paymentIntentSucceeded = event.data.object;
+
+        const order = await Order.findById(paymentIntentSucceeded.metadata.orderId);
+        order.paymentStatus = 'received';
+        await order.save();
+
+        // Redirect to the home page
+        response.redirect('/'); // Change '/' to the desired URL
+        
+        break;
+      // ... handle other event types
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+
+    // Return a 200 response to acknowledge receipt of the event
+    response.send();
+  }
+);
+
+
+console.log(process.env.PORT);
+
+// JWT options
+const opts = {};
+opts.jwtFromRequest = cookieExtractor;
+opts.secretOrKey = process.env.JWT_SECRET_KEY;
+
+// Middlewares
+server.use(cookieParser());
+server.use(
+  session({
+    secret: process.env.SESSION_KEY,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+server.use(passport.initialize());
+server.use(passport.session());
+server.use(
+  cors({
+    exposedHeaders: ['X-Total-Count'],
+  })
+);
+server.use(express.json()); // to parse req.body
+
+server.use('/backend/products', passport.authenticate('jwt'), productsRouter.router);
+server.use('/backend/categories', passport.authenticate('jwt'), categoriesRouter.router);
+server.use('/backend/brands', passport.authenticate('jwt'), brandsRouter.router);
+server.use('/backend/users',  usersRouter.router);
+server.use('/backend/auth', authRouter.router);
+server.use('/backend/cart', passport.authenticate('jwt'), cartRouter.router);
+server.use('/backend/orders', passport.authenticate('jwt'), ordersRouter.router);
+
+// this line we add to make react router work in case other routes don't match
+server.get('*', (req, res) =>
+  res.sendFile(path.resolve('build', 'index.html'))
+);
+
+// Passport Strategies
+passport.use(
+  'local',
+  new LocalStrategy({ usernameField: 'email' }, async function (
+    email,
+    password,
+    done
+  ) {
+    console.log({ email, password });
+    try {
+      const user = await User.findOne({ email: email });
+      console.log(email, password, user);
+      if (!user) {
+        return done(null, false, { message: 'invalid credentials' });
+      }
+      crypto.pbkdf2(
+        password,
+        user.salt,
+        310000,
+        32,
+        'sha256',
+        async function (err, hashedPassword) {
+          if (!crypto.timingSafeEqual(user.password, hashedPassword)) {
+            return done(null, false, { message: 'invalid credentials' });
+          }
+          const token = jwt.sign(
+            sanitizeUser(user),
+            process.env.JWT_SECRET_KEY
+          );
+          done(null, { id: user.id, role: user.role, token });
+        }
+      );
+    } catch (err) {
+      done(err);
+    }
+  })
+);
+
+passport.use(
+  'jwt',
+  new JwtStrategy(opts, async function (jwt_payload, done) {
+    try {
+      const user = await User.findById(jwt_payload.id);
+      if (user) {
+        return done(null, sanitizeUser(user));
+      } else {
+        return done(null, false);
+      }
+    } catch (err) {
+      return done(err, false);
+    }
+  })
+);
+
+passport.serializeUser(function (user, cb) {
+  process.nextTick(function () {
+    return cb(null, { id: user.id, role: user.role });
+  });
+});
+
+passport.deserializeUser(function (user, cb) {
+  process.nextTick(function () {
+    return cb(null, user);
+  });
+});
+
+// Payments
+const stripe = require('stripe')(process.env.STRIPE_SERVER_KEY);
+
+server.post('/backend/create-payment-intent', async (req, res) => {
+  const { totalAmount, orderId } = req.body;
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: totalAmount * 100,
+    currency: 'inr',
+    automatic_payment_methods: {
+      enabled: true,
+    },
+    metadata: {
+      orderId,
+    },
+  });
+
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log('database connected');
+}
+
+server.listen(process.env.PORT, () => {
+  console.log('server started');
+});
