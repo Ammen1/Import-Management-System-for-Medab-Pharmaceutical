bd778e39be20d99e0d99545f92c5534dce96b944
Tamirat <amenguda@gmail.com>
2024-03-14 11:10:49 +0300

    create Dashboard for all users

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import DashProfile from '../Components/DashProfile';
+import DashPosts from '../Components/DashPosts';
+import DashUsers from '../Components/DashUsers';
+import DashboardComp from '../Components/DashboardComp';
+import DashSidebar from '../Components/DashSidebar';
+
+export default function Dashboard() {
+  const location = useLocation();
+  const [tab, setTab] = useState('');
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab');
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+  return (
+    <div className='min-h-screen flex flex-col md:flex-row  '>
+      <div className='md:w-56'>
+        <DashSidebar />
+      </div>
+   
+      {tab === 'profile' && <DashProfile />}
+      {tab === 'posts' && <DashPosts />}
+      {tab === 'users' && <DashUsers />}  
+      {tab === 'dash' && <DashboardComp />}
+ 
+    </div>
+  );
+}
