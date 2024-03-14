f33ee980166270dbc97c368622b1c6dddca7df8c
Tamirat <amenguda@gmail.com>
2024-03-14 11:15:11 +0300

    user crud added

---
+++ backend/controller/User.js
@@ -0,0 +1,32 @@
+const { Category } = require('../model/Category');
+const { User } = require('../model/User');
+
+exports.fetchUserById = async (req, res) => {
+  const { id } = req.user;
+  console.log(id)
+  try {
+    const user = await User.findById(id);
+    res.status(200).json({id:user.id,addresses:user.addresses,email:user.email,role:user.role});
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
+
+exports.fechUser = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error){
+    res.status(400).json(err);
+  }
+  };
+
+exports.updateUser = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await User.findByIdAndUpdate(id, req.body, { new: true });
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
