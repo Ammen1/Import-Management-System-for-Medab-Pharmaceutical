9db961284f6d5082c498f3ceb09e10d521c65b36
Tamirat <amenguda@gmail.com>
2024-03-14 20:26:49 +0300

    Add Header component
    Added a Header component responsible for rendering the navigation bar at the top of the application. The Header includes links to the home page, a search input field with search functionality, and a button for toggling the theme between light and dark mode. The component also handles user sign-out functionality.
    
    Fixes: 1

---
+++ client/src/Components/Header.jsx
@@ -0,0 +1,87 @@
+import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { useSelector, useDispatch } from 'react-redux';
+import { signoutSuccess } from '../redux/user/userSlice';
+import { useEffect, useState } from 'react';
+import { FaSun, FaMoon } from 'react-icons/fa'; 
+import userSlice from '../features/user/userSlice';
+
+export default function Header() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
+
+  const handleSignout = async () => {
+    try {
+      const res = await fetch('/api/user/signout', {
+        method: 'POST',
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signoutSuccess());
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  return (
+    <Navbar className='border-b-2 p-2'>
+      <Link
+        to='/'
+        className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'
+      >
+        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 rounded-lg text-white'>
+        Medab 
+        </span>
+        Pharmaceutical 
+      </Link>
+      <form onSubmit={handleSubmit}>
+        <TextInput
+          type='text'
+          placeholder='Search...'
+          rightIcon={AiOutlineSearch}
+          className='hidden lg:inline dark:bg-black'
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          
+        />
+      </form>
+      <Button className='w-12 h-10 lg:hidden' color='gray' pill>
+        <AiOutlineSearch />
+      </Button>
+      <div className='flex gap-2 md:order-2'>
+        <Button
+          className='w-12 h-12 hidden sm:inline border rounded-full'
+          color='black'
+          pill
+          // onClick={() => dispatch(toggleTheme())}
+        >
+          {userSlice === true ? <FaSun /> : <FaMoon />}
+        </Button>
+    
+      </div>
+        </Navbar>
+  );
+}
