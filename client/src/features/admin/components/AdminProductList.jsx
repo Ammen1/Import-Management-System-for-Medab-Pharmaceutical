0a5499fddb2b416ea3ee9275daa009910ab39bae
Tamirat <amenguda@gmail.com>
2024-03-14 20:20:45 +0300

    Add Tailwind CSS configuration file
    Added a Tailwind CSS configuration file named 'tailwind.config.js' to the project root. This file specifies the content to be processed by Tailwind CSS, including HTML, JavaScript, TypeScript, and JSX files in the 'src' directory, as well as files from the 'flowbite-react' package. Additionally, the configuration extends the default theme and includes plugins for 'flowbite' and 'tailwind-scrollbar'.
    
    Fixes: 3 (if applicable)

--- client/src/features/admin/components/AdminProductList.jsx
+++ client/src/features/admin/components/AdminProductList.jsx
@@ -1,4 +1,4 @@
-import React, { useState, Fragment, useEffect } from 'react';
+import { useState, Fragment, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import {
   fetchBrandsAsync,
