8901bf05d046b1982107d1e8fdb3ca43c498462a
Tamirat <amenguda@gmail.com>
2024-03-15 00:06:49 +0300

    Added AdminProductList component to Products page

---
+++ client/src/Components/Products.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import AdminProductList from '../features/admin/components/AdminProductList'
+
+export default function Products() {
+  return (
+    <div><AdminProductList /></div>
+  )
+}
