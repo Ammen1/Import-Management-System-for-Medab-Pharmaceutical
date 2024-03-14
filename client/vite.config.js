d3d79e8039fa030d81855e9f8d7fd0b2fc5c5fcf
Tamirat <amenguda@gmail.com>
2024-03-14 11:21:38 +0300

    update proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,9 +4,10 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    port: 3000,
     proxy: {
-      "/api": {
-        target: "http://localhost:3000",
+      '/backend': {
+        target: 'http://localhost:8080', // Update with your backend server URL
         secure: false,
       },
     },
