6c4f27bed7af341220b77a4d8f7e04d80cb0ab74
Tamirat <amenguda@gmail.com>
2024-03-15 21:52:13 +0300

    Add Orders component to render AdminOrders

---
+++ client/src/Components/Orders.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import AdminOrders from '../features/admin/components/AdminOrders'
+
+export default function Orders() {
+  return (
+    <div><AdminOrders /></div>
+  )
+}
