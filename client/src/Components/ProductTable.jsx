40ed08710f22c63066d54d5715c451c020a989c2
Tamirat <amenguda@gmail.com>
2024-03-16 00:40:54 +0300

    Remove unnecessary line code and clean up the codebase.

--- client/src/Components/ProductTable.jsx
+++ client/src/Components/ProductTable.jsx
@@ -38,38 +38,42 @@ function classNames(...classes) {
 
 function ProductTable({ products }) {
     return (
-        <div className="bg-white table-auto overflow-hidden lg:mt-12 overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
-        <div className="mx-auto max-w-2xl px-4 py-0 sm:px-6 sm:py-0 lg:max-w-7xl lg:px-8 ml-20">
-          <table className="min-w-full divide-y ml-16  divide-gray-200">
-            <thead className="bg-gray-50 ">
+        <div className=" max-w-screen-lg overflow-hidden mt-12 overflow-x-scroll  scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 scrollbar-track-slate-700 scrollbar-thumb-slate-500 ">
+        <div className="mx-auto max-w-2xl px-4 py-0 sm:px-6 sm:py-0 lg:max-w-7xl ">
+          <table className=" max-w-screen-lg  divide-y   divide-gray-200">
+            <thead className="bg-gray-50  ">
               <tr>
                 <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Brand</th>
+                <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Thumbnail</th>
                 <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Category</th>
                 <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Regulatory Info</th>
                 <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Product Specifications</th>
                 <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Safety Info</th>
                 <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Batch Number</th>
                 <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Expiry Date</th>
-                <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Thumbnail</th>
                 <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Edit</th>
-                <th scope="col" className="relative px-6 py-3"><span className="sr-only">Edit</span></th>
+                <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Delete</th>
               </tr>
             </thead>
             <tbody className="bg-white divide-y divide-gray-200">
               {products.map((product) => (
                 <tr key={product.id}>
                   <td className="px-6 py-4 whitespace-nowrap">{product.brand}</td>
+                  <td className="px-6 py-4 whitespace-nowrap">
+                    <img className="h-10 w-10 rounded-full" src={product.thumbnail} alt={product.title} />
+                  </td>
                   <td className="px-6 py-4 whitespace-nowrap">{product.category}</td>
                   <td className="px-6 py-4 whitespace-nowrap">{product.regulatoryInfo}</td>
                   <td className="px-6 py-4 whitespace-nowrap">{product.productSpecifications}</td>
                   <td className="px-6 py-4 whitespace-nowrap">{product.safetyInfo}</td>
                   <td className="px-6 py-4 whitespace-nowrap">{product.batchNumber}</td>
                   <td className="px-6 py-4 whitespace-nowrap">{new Date(product.expiryDate).toLocaleDateString()}</td>
-                  <td className="px-6 py-4 whitespace-nowrap">
-                    <img className="h-10 w-10 rounded-full" src={product.thumbnail} alt={product.title} />
-                  </td>
+
                   <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
                     <Link to={`/admin/product-form/edit/${product.id}`} className="text-indigo-600 hover:text-indigo-900">Edit</Link>
+                  </td>
+                  <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                    <Link to={`/admin/product-form/edit/${product.id}`} className="text-indigo-600 hover:text-indigo-900">Delete</Link>
                   </td>
                 </tr>
               ))}
@@ -99,31 +103,9 @@ export default function AdminProductList() {
     },
   ];
 
-  const [filter, setFilter] = useState({});
-  const [sort, setSort] = useState({});
+  const [filter] = useState({});
+  const [sort] = useState({});
   const [page, setPage] = useState(1);
-
-  const handleFilter = (e, section, option) => {
-    const newFilter = { ...filter };
-    if (e.target.checked) {
-      if (newFilter[section.id]) {
-        newFilter[section.id].push(option.value);
-      } else {
-        newFilter[section.id] = [option.value];
-      }
-    } else {
-      const index = newFilter[section.id].findIndex(
-        (el) => el === option.value
-      );
-      newFilter[section.id].splice(index, 1);
-    }
-    setFilter(newFilter);
-  };
-
-  const handleSort = (e, option) => {
-    const sort = { _sort: option.sort, _order: option.order };
-    setSort(sort);
-  };
 
   const handlePage = (page) => {
     setPage(page);
@@ -144,16 +126,14 @@ export default function AdminProductList() {
   }, []);
 
   return (
-    <div className="bg-white w-screen">
+    <div className="bg-white  ">
       <div>
-        <main className="mx-auto max-w-7xl  px-4 sm:px-6 lg:px-8 ">
-          <div className="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-24">
-          </div>
+        <main className=" mt-28 ">
             <div className="lg:col-span-3">
-              <div>
+              <div className=' ml-10'>
                 <Link
                   to="/dashboard?tab=add-product"
-                  className="rounded-md  mx-10  bg-green-700 px-3 py-2 text-sm font-semibold ml-56 text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  className="rounded-md  px-3 py-2 text-sm font-semibold  text-white shadow-sm bg-gradient-to-r from-indigo-500 to-pink-600 via-purple-600"
                 >
                   Add New Product
                 </Link>
@@ -171,7 +151,6 @@ export default function AdminProductList() {
     </div>
   );
 }
-
 
 function Pagination({ page, setPage, handlePage, totalItems }) {
   const totalPages = Math.ceil(totalItems / ITEMS_PER_PAGE);
