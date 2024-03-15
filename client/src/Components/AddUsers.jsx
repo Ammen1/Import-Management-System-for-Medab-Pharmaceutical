51b369dc43422ebac039ac1fff10842239de81fb
Tamirat <amenguda@gmail.com>
2024-03-15 22:02:54 +0300

    "Implement AddUsers component with user creation form"
    Description:
    Added the AddUsers component, providing a form for creating new user accounts. Utilized React Redux for state management, React Hook Form for form validation, and react-router-dom for navigation. The form includes fields for email, password, confirm password, and role selection. Upon submission, the createUserAsync action is dispatched to create the user asynchronously. Styling and validation messages are provided for better user experience.

---
+++ client/src/Components/AddUsers.jsx
@@ -0,0 +1,183 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { useForm } from 'react-hook-form';
+import { Link, Navigate } from 'react-router-dom';
+
+import { selectLoggedInUser, createUserAsync } from '../features/auth/authSlice';
+import { Button } from 'flowbite-react';
+import { useNavigate } from 'react-router-dom';
+
+export default function AddUsers() {
+    const history = useNavigate()
+  const dispatch = useDispatch();
+  const user = useSelector(selectLoggedInUser);
+  
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    await dispatch(createUserAsync({
+      email: data.email,
+      password: data.password,
+      addresses: [],
+      role: data.role, // If role is not provided, default to 'user'
+    }));
+    history('/dashboard?tab=dash');
+  };
+
+  return (
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-20 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            className="mx-auto h-16 mt-5 w-auto"
+            src="/logo.png"
+            alt="Your Company"
+          />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Create a New Account
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form
+            noValidate
+            className="space-y-6"
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  {...register('email', {
+                    required: 'email is required',
+                    pattern: {
+                      value: /\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/gi,
+                      message: 'email not valid',
+                    },
+                  })}
+                  type="email"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+                {errors.email && (
+                  <p className="text-red-500">{errors.email.message}</p>
+                )}
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Password
+              </label>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  {...register('password', {
+                    required: 'password is required',
+                    pattern: {
+                      value:
+                        /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm,
+                      message: `- at least 8 characters\n
+                      - must contain at least 1 uppercase letter, 1 lowercase letter, and 1 number\n
+                      - Can contain special characters`,
+                    },
+                  })}
+                  type="password"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+                {errors.password && (
+                  <p className="text-red-500">{errors.password.message}</p>
+                )}
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="confirmPassword"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Confirm Password
+              </label>
+              <div className="mt-2">
+                <input
+                  id="confirmPassword"
+                  {...register('confirmPassword', {
+                    required: 'confirm password is required',
+                    validate: (value, formValues) =>
+                      value === formValues.password || 'password not matching',
+                  })}
+                  type="password"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+                {errors.confirmPassword && (
+                  <p className="text-red-500">
+                    {errors.confirmPassword.message}
+                  </p>
+                )}
+              </div>
+            </div>
+
+            {/* Role select field */}
+            <div>
+              <label
+                htmlFor="role"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Role
+              </label>
+              <div className="mt-2">
+                <select
+                  id="role"
+                  {...register('role', {
+                    required: 'Role is required',
+                  })}
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                >
+                  <option value="">Select a role</option>
+                  <option value="Distributers">Distributers</option>
+                  <option value="Manager">Manager</option>
+                  <option value="Suppliers">Suppliers</option>
+                </select>
+                {errors.role && (
+                  <p className="text-red-500">{errors.role.message}</p>
+                )}
+              </div>
+            </div>
+
+            <div>
+              <Button
+              pill
+              processingLabel='amen'
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-gradient-to-b from-indigo-500 to-pink-600 via-purple-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign Up
+              </Button>
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm text-gray-500">
+            Already a Member?{' '}
+            <Link
+              to="/login"
+              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+            >
+              Log In
+            </Link>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
