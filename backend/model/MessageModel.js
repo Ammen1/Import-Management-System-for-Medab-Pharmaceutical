e569edb7192c3c0120c2c98135dfdb5f75ef740b
Tamirat <amenguda@gmail.com>
2024-04-14 13:37:10 +0300

    Add Message schema and model
    This commit introduces a Message schema and model to the application.
    
    - The Message schema defines the structure of a message, including fields for chatId, senderId, and text, with timestamps enabled.
    - The Message model is created using the mongoose.model function, allowing messages to be stored and queried in the database.
    
    These additions will facilitate the storage and retrieval of messages in the application.

---
+++ backend/model/MessageModel.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+const MessageSchema = new mongoose.Schema(
+  {
+    chatId: {
+      type: String,
+    },
+    senderId: {
+      type: String,
+    },
+    text: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+exports.MessageModel = mongoose.model("Message", MessageSchema);
+
