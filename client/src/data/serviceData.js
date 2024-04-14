85582c29cf49c84fd20a79af4b7213db862c4efa
Tamirat <amenguda@gmail.com>
2024-04-14 13:43:11 +0300

    Add service data
    This commit adds a JavaScript file named serviceData.js, which contains an array of objects representing various services offered by the application. Each object includes properties such as id, title, icon, description, and image, providing essential information about each service.
    
    These service data will be used to populate service-related components and functionalities within the application.

---
+++ client/src/data/serviceData.js
@@ -0,0 +1,65 @@
+// serviceData.js
+
+const serviceData = [
+    {
+      id: 1,
+      title: "Log in to our provider portal",
+      icon: "public/assets/services/001-portal.png",
+      description: "The provider portal has information and tools to complete real-time transactions along with administrative and financial tools. You can: ipsum dolor sit amet consectetur adipisicing elit. Fuga quisquam ducimus quia! Et excepturi ut voluptatem. Fugit, perferendis expedita dolorum non veritatis repudiandae minima recusandae maxime corrupti a sunt dicta accusamus exercitationem molestias totam, et voluptatem laboriosam ducimus quis dignissimos, accusantium in. Corrupti, aperiam! Hic facilis inventore quaerat consectetur praesentium eius, itaque, tempora laborum, perferendis eos enim qui dolore quisquam libero porro vero quasi? Quam quia laborum earum quisquam cum officiis, dicta voluptate perspiciatis dolores molestiae eum maxime aliquid iste sunt hic, facilis recusandae ducimus. Neque similique, vero ea possimus eos totam aliquid repudiandae eum enim quasi harum, corporis sequi!",
+      image: "public/assets/services/image/service-1.jpg"
+    },
+    {
+      id: 2,
+      title: "Prescribing Tools",
+      icon: "public/assets/services/004-tools.png",
+      description: "Search our formulary list of preferred drugs. Find a pharmacy clinical policy bulletin, or search our preferred drug lists. Or find out more about e-prescribing.Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga quisquam ducimus quia! Et excepturi ut voluptatem. Fugit, perferendis expedita dolorum non veritatis repudiandae minima recusandae maxime corrupti a sunt dicta accusamus exercitationem molestias totam, et voluptatem laboriosam ducimus quis dignissimos, accusantium in. Corrupti, aperiam! Hic facilis inventore quaerat consectetur praesentium eius, itaque, tempora laborum, perferendis eos enim qui dolore quisquam libero porro vero quasi? Quam quia laborum earum quisquam cum officiis, dicta voluptate perspiciatis dolores molestiae eum maxime aliquid iste sunt hic, facilis recusandae ducimus. Neque similique, vero ea possimus eos totam aliquid repudiandae eum enim quasi harum, corporis sequi!",
+      image: "/path/to/image2.jpg"
+    },
+    {
+      id: 3,
+      title: "Pharmacy Claims",
+      icon: "public/assets/services/002-medicine.png",
+      description: "Search our formulary list of preferred drugs. Find a pharmacy clinical policy bulletin, or search our preferred drug lists. Or find out more about e-prescribing.Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga quisquam ducimus quia! Et excepturi ut voluptatem. Fugit, perferendis expedita dolorum non veritatis repudiandae minima recusandae maxime corrupti a sunt dicta accusamus exercitationem molestias totam, et voluptatem laboriosam ducimus quis dignissimos, accusantium in. Corrupti, aperiam! Hic facilis inventore quaerat consectetur praesentium eius, itaque, tempora laborum, perferendis eos enim qui dolore quisquam libero porro vero quasi? Quam quia laborum earum quisquam cum officiis, dicta voluptate perspiciatis dolores molestiae eum maxime aliquid iste sunt hic, facilis recusandae ducimus. Neque similique, vero ea possimus eos totam aliquid repudiandae eum enim quasi harum, corporis sequi!",
+      image: "/path/to/image3.jpg"
+    },
+
+    {
+      id: 2,
+      title: "Pharmacy Help Line",
+      icon: "public/assets/services/005-hot-line.png",
+      description: "Do you need answers to questions about prescription drugs? We’re here 24 hours a day, every day.Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga quisquam ducimus quia! Et excepturi ut voluptatem. Fugit, perferendis expedita dolorum non veritatis repudiandae minima recusandae maxime corrupti a sunt dicta accusamus exercitationem molestias totam, et voluptatem laboriosam ducimus quis dignissimos, accusantium in. Corrupti, aperiam! Hic facilis inventore quaerat consectetur praesentium eius, itaque, tempora laborum, perferendis eos enim qui dolore quisquam libero porro vero quasi? Quam quia laborum earum quisquam cum officiis, dicta voluptate perspiciatis dolores molestiae eum maxime aliquid iste sunt hic, facilis recusandae ducimus. Neque similique, vero ea possimus eos totam aliquid repudiandae eum enim quasi harum, corporis sequi!",
+      image: "/path/to/image2.jpg"
+    },
+    {
+      id: 3,
+      title: "Specialty Medications",
+      icon: "public/assets/services/006-medical-care.png",
+      description: "Some patients require specialty drugs for chronic or complex conditions. The network specialty pharmacy can dispense these drugs quickly and at a reasonable cost..Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga quisquam ducimus quia! Et excepturi ut voluptatem. Fugit, perferendis expedita dolorum non veritatis repudiandae minima recusandae maxime corrupti a sunt dicta accusamus exercitationem molestias totam, et voluptatem laboriosam ducimus quis dignissimos, accusantium in. Corrupti, aperiam! Hic facilis inventore quaerat consectetur praesentium eius, itaque, tempora laborum, perferendis eos enim qui dolore quisquam libero porro vero quasi? Quam quia laborum earum quisquam cum officiis, dicta voluptate perspiciatis dolores molestiae eum maxime aliquid iste sunt hic, facilis recusandae ducimus. Neque similique, vero ea possimus eos totam aliquid repudiandae eum enim quasi harum, corporis sequi!",
+      image: "/path/to/image3.jpg"
+    },
+    {
+      id: 1,
+      title: "Specialty Medications",
+      icon: "public/assets/services/003-tool-box.png",
+      description: "Some patients require specialty drugs for chronic or complex conditions. The network specialty pharmacy can dispense these drugs quickly and at a reasonable cost..Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga quisquam ducimus quia! Et excepturi ut voluptatem. Fugit, perferendis expedita dolorum non veritatis repudiandae minima recusandae maxime corrupti a sunt dicta accusamus exercitationem molestias totam, et voluptatem laboriosam ducimus quis dignissimos, accusantium in. Corrupti, aperiam! Hic facilis inventore quaerat consectetur praesentium eius, itaque, tempora laborum, perferendis eos enim qui dolore quisquam libero porro vero quasi? Quam quia laborum earum quisquam cum officiis, dicta voluptate perspiciatis dolores molestiae eum maxime aliquid iste sunt hic, facilis recusandae ducimus. Neque similique, vero ea possimus eos totam aliquid repudiandae eum enim quasi harum, corporis sequi!",
+      image: "/path/to/image1.jpg"
+    },
+    {
+      id: 2,
+      title: "Our URAC pharmacy accreditations",
+      icon: "public/assets/services/007-injection.png",
+      description: "URAC is an independent, non-profit health care accrediting organization dedicated to promoting health care quality through accreditation, education, and measurement.Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga quisquam ducimus quia! Et excepturi ut voluptatem. Fugit, perferendis expedita dolorum non veritatis repudiandae minima recusandae maxime corrupti a sunt dicta accusamus exercitationem molestias totam, et voluptatem laboriosam ducimus quis dignissimos, accusantium in. Corrupti, aperiam! Hic facilis inventore quaerat consectetur praesentium eius, itaque, tempora laborum, perferendis eos enim qui dolore quisquam libero porro vero quasi? Quam quia laborum earum quisquam cum officiis, dicta voluptate perspiciatis dolores molestiae eum maxime aliquid iste sunt hic, facilis recusandae ducimus. Neque similique, vero ea possimus eos totam aliquid repudiandae eum enim quasi harum, corporis sequi!",
+      image: "/path/to/image2.jpg"
+    },
+    {
+      id: 3,
+      title: "Specialty pharmacy",
+      icon: "public/assets/services/001-portal.png",
+      description: "Some patients require specialty drugs for chronic or complex conditions. The network specialty pharmacy can dispense these drugs quickly and at a reasonable cost..Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga quisquam ducimus quia! Et excepturi ut voluptatem. Fugit, perferendis expedita dolorum non veritatis repudiandae minima recusandae maxime corrupti a sunt dicta accusamus exercitationem molestias totam, et voluptatem laboriosam ducimus quis dignissimos, accusantium in. Corrupti, aperiam! Hic facilis inventore quaerat consectetur praesentium eius, itaque, tempora laborum, perferendis eos enim qui dolore quisquam libero porro vero quasi? Quam quia laborum earum quisquam cum officiis, dicta voluptate perspiciatis dolores molestiae eum maxime aliquid iste sunt hic, facilis recusandae ducimus. Neque similique, vero ea possimus eos totam aliquid repudiandae eum enim quasi harum, corporis sequi!",
+      image: "/path/to/image3.jpg"
+    },
+  
+  ];
+  
+  export default serviceData;
+  
