bd3940fc522a848d2cee46e9c070583c32022b81
Tamirat <amenguda@gmail.com>
2024-04-15 20:56:42 +0300

    feat: Added Services component to display service cards in a swiper
    - Implemented Services component to display service cards using Swiper for pagination
    - Utilized chunkArray function to split service data into chunks for Swiper slides
    - Applied Bounce effect from react-reveal for animation on service card entry

---
+++ client/src/Components/Services/Services.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import Bounce from 'react-reveal/Bounce';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import Heading from '../Heading';
+import Service from './Service';
+import serviceData from '../../data/serviceData'; 
+
+const chunkArray = (array, chunkSize) => {
+    const chunkedArray = [];
+    for (let i = 0; i < array.length; i += chunkSize) {
+        chunkedArray.push(array.slice(i, i + chunkSize));
+    }
+    return chunkedArray;
+};
+
+const Services = () => {
+    const chunkedServiceData = chunkArray(serviceData, 4);
+
+    return (
+        <section className="max-w-screen-xl mx-auto px-6 py-6 pb-24">
+            {/* heading  */}
+            <Heading title="Services" />
+
+            {/* Swiper for pagination */}
+            <Swiper
+                className="mySwiper py-12 "
+                slidesPerView={1}
+                spaceBetween={40}
+                pagination={{ clickable: true }}
+            >
+                {chunkedServiceData.map((chunk, index) => (
+                    <SwiperSlide key={index}>
+                        <div className="grid grid-cols-1  sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-8 ">
+                            {chunk.map(service => (
+                                <Bounce left key={service.id}>
+                                    <Service {...service} />
+                                </Bounce>
+                            ))}
+                        </div>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </section>
+    );
+};
+
+export default Services;
