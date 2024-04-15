5782d9c6235c8fe06beff5dfc2bd370c2ac65239
Tamirat <amenguda@gmail.com>
2024-04-15 21:27:21 +0300

    feat: Added components to Home page layout
    - Integrated Navbar, Banner, Coffee, Component, Services, ContactScreen, and Footer components into the Home page layout

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,16 +5,23 @@ import Footer from "../features/common/Footer";
 import Banner from "../Components/Header/Banner";
 import Services from "../Components/Services/Services";
 import ContactScreen from "../screens/ContactScreen";
+import Testimonial from "../Components/Testimonial/Testimonial";
+import Coffee from "./Coffee";
+import Component from "../Components/Header/Componen";
 function Home() {
     return ( 
         <div className="">
             <NavBar>
                 <Banner></Banner>
                 <div className=" ">
+                <Coffee></Coffee>
+                <Component></Component>
                 <Services></Services>
                 </div>
-                
+
+
                 <ContactScreen></ContactScreen>
+                
             </NavBar>
             <Footer></Footer>
         </div>
