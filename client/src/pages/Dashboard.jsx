3e1895ccac1744d3f456d46201527334b1768994
Tamirat <amenguda@gmail.com>
2024-07-17 17:59:06 +0300

    "Removed files backend/medicine_order_report.pdf, backend/model/Product.js, client/src/App.jsx, client/src/Components/AddUsers.jsx, client/src/Components/DashboardComp.jsx, and client/src/pages/Dashboard.jsx; modified schema in Product.js

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -34,7 +34,7 @@ export default function Dashboard() {
     }
   }, [location.search]);
   return (
-    <div className='min-h-screen flex flex-col md:flex-row  '>
+    <div className='min-h-screen flex flex-col md:flex-row '>
       <div className='md:w-56'>
         <DashSidebar />
       </div>
